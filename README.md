a11b8ef3a62699a4545340111bcacf675b9d1ae6
Jkgdev8 <juliennegarcia@Juliennes-MBP.socal.rr.com>
2022-01-11 17:46:04 -0800

    completed get routes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Note Taker Starter Code# Note-Taker-UCLA-Challenge-11
+An application that can be used to write, save, and delete notes. This application will use an express backend and save and retrieve note data from a JSON file.
+
+![bg-showcase-3](example.png)
+
+
+
+# Tech Used
+js, node, express, npm modules
--- server.js
+++ server.js
@@ -2,3 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+require('./routing/apiroute')(app);
+require('./routing/htmlroute')(app);
+
+
+app.listen(PORT, function() {
+ console.log(`Server is listening on PORT: ${PORT}`);
+});
